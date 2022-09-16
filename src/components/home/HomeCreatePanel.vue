f91cbd8a1c192b332924c6c57d795f2b6374d57e
besoeasy <besoeasy@users.noreply.github.com>
2022-09-16 06:14:00 +0530

    Redesign /new page with full-page layout and cleaner forms
    Match vault page shell, remove stacked ui-panel boxes, improve mode
    toggle and invite/pubkey sections, add back link to messages.

--- src/components/home/HomeCreatePanel.vue
+++ src/components/home/HomeCreatePanel.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PrimaryButton from "@/components/PrimaryButton.vue";
+import { KeyRound, Users } from "lucide-vue-next";
 
 defineProps({
   activePanel: { type: String, default: "" },
@@ -20,40 +21,68 @@ const emit = defineEmits([
 </script>
 
 <template>
-  <div v-if="activePanel === 'dm'" class="ui-panel rounded-2xl p-4 space-y-3">
-    <p class="text-xs font-semibold text-zinc-400">New Message</p>
-    <input
-      :value="dmPubkey"
-      placeholder="Recipient public key (64 or 66 hex chars)"
-      class="chat-input-modern w-full rounded-2xl px-4 py-2.5 text-sm placeholder-zinc-500 focus:outline-none transition-colors"
-      @input="emit('update:dmPubkey', $event.target.value)"
-    />
-    <PrimaryButton @click="emit('create-dm')" :loading="openingDm">
-      {{ openingDm ? "Opening…" : "Open Conversation" }}
-    </PrimaryButton>
-    <p class="text-zinc-500 text-[11px] text-center">End-to-end encrypted</p>
-  </div>
+  <form
+    v-if="activePanel === 'dm'"
+    class="space-y-5"
+    @submit.prevent="emit('create-dm')"
+  >
+    <div>
+      <label class="mb-1.5 flex items-center gap-1.5 text-sm font-medium text-zinc-300">
+        <KeyRound class="h-4 w-4 text-zinc-500" aria-hidden="true" />
+        Recipient public key
+      </label>
+      <input
+        :value="dmPubkey"
+        placeholder="64-char x-only or 66-char compressed hex key"
+        class="ui-input w-full font-mono text-sm"
+        autocomplete="off"
+        spellcheck="false"
+        @input="emit('update:dmPubkey', $event.target.value)"
+      />
+      <p class="mt-2 text-xs leading-relaxed text-zinc-500">
+        Paste their Nostr public key to open an end-to-end encrypted direct message.
+      </p>
+    </div>
 
-  <div v-else-if="activePanel === 'group'" class="ui-panel rounded-2xl p-4 space-y-3">
-    <p class="text-xs font-semibold text-zinc-400">Create Group</p>
-    <input
-      :value="name"
-      placeholder="Group name"
-      class="chat-input-modern w-full rounded-2xl px-4 py-2.5 text-sm placeholder-zinc-500 focus:outline-none transition-colors"
-      @input="emit('update:name', $event.target.value)"
-    />
-    <textarea
-      :value="description"
-      rows="2"
-      placeholder="Short description (optional)"
-      class="chat-input-modern w-full rounded-2xl px-4 py-2.5 text-sm placeholder-zinc-500 focus:outline-none resize-none transition-colors"
-      @input="emit('update:description', $event.target.value)"
-    />
-    <PrimaryButton @click="emit('create-group')" :loading="saving">
-      {{ saving ? "Creating…" : "Create Group" }}
+    <PrimaryButton type="submit" :loading="openingDm">
+      {{ openingDm ? "Opening…" : "Open conversation" }}
     </PrimaryButton>
-    <p class="text-zinc-500 text-[11px] text-center">
-      Invite people later with automatic epoch rotation
-    </p>
-  </div>
+  </form>
+
+  <form
+    v-else-if="activePanel === 'group'"
+    class="space-y-5"
+    @submit.prevent="emit('create-group')"
+  >
+    <div>
+      <label class="mb-1.5 flex items-center gap-1.5 text-sm font-medium text-zinc-300">
+        <Users class="h-4 w-4 text-zinc-500" aria-hidden="true" />
+        Group name
+      </label>
+      <input
+        :value="name"
+        placeholder="e.g. Project crew, Family, Ops"
+        class="ui-input w-full"
+        @input="emit('update:name', $event.target.value)"
+      />
+    </div>
+
+    <div>
+      <label class="mb-1.5 block text-sm font-medium text-zinc-300">Description (optional)</label>
+      <textarea
+        :value="description"
+        rows="3"
+        placeholder="What is this group for?"
+        class="ui-input min-h-[88px] w-full resize-y"
+        @input="emit('update:description', $event.target.value)"
+      />
+      <p class="mt-2 text-xs leading-relaxed text-zinc-500">
+        You can invite members after the group is created. Epoch rotation keeps membership private.
+      </p>
+    </div>
+
+    <PrimaryButton type="submit" :loading="saving">
+      {{ saving ? "Creating…" : "Create group" }}
+    </PrimaryButton>
+  </form>
 </template>
--- src/views/NewChatView.vue
+++ src/views/NewChatView.vue
@@ -1,7 +1,16 @@
 <script setup>
-import { computed, ref, watch } from "vue";
+import { computed, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { Check, Clock, Copy, Link2, MessageCircle, RefreshCw, Users } from "lucide-vue-next";
+import {
+  ArrowLeft,
+  Check,
+  Clock,
+  Copy,
+  Link2,
+  MessageCircle,
+  RefreshCw,
+  Users,
+} from "lucide-vue-next";
 import AppAlertBanner from "@/components/AppAlertBanner.vue";
 import HomeCreatePanel from "@/components/home/HomeCreatePanel.vue";
 import PrimaryButton from "@/components/PrimaryButton.vue";
@@ -30,18 +39,24 @@ const initPromise = identity.init().then(() => {
 });
 
 const modes = [
-  { id: "dm", label: "Message", icon: MessageCircle },
-  { id: "group", label: "Group", icon: Users },
+  {
+    id: "dm",
+    label: "Direct message",
+    shortLabel: "Message",
+    icon: MessageCircle,
+    description: "Open a 1:1 encrypted chat with a public key.",
+  },
+  {
+    id: "group",
+    label: "Private group",
+    shortLabel: "Group",
+    icon: Users,
+    description: "Create a room and invite people after it opens.",
+  },
 ];
 
-const eyebrow = computed(() => (mode.value === "group" ? "Create Group" : "New Message"));
 const title = computed(() =>
   mode.value === "group" ? "Start a private group" : "Start a direct chat",
-);
-const subtitle = computed(() =>
-  mode.value === "group"
-    ? "Create a group room now. You can invite people after it opens."
-    : "Paste a 64-char x-only or 66-char compressed public key to open an encrypted conversation.",
 );
 
 const copied = ref(false);
@@ -54,6 +69,10 @@ const activeInvite = ref(null);
 const selectedTtl = computed(
   () =>
     INVITE_TTL_OPTIONS.find((option) => option.id === selectedTtlId.value) || INVITE_TTL_OPTIONS[1],
+);
+
+const activeModeMeta = computed(
+  () => modes.find((item) => item.id === mode.value) || modes[0],
 );
 
 function flashCopied(state) {
@@ -88,14 +107,6 @@ async function generateInvite() {
     inviteBusy.value = false;
   }
 }
-
-watch(
-  () => route.query.type,
-  (type) => {
-    mode.value = type === "group" ? "group" : "dm";
-    error.value = "";
-  },
-);
 
 function setMode(nextMode) {
   if (mode.value === nextMode) return;
@@ -161,94 +172,100 @@ async function createDM() {
 </script>
 
 <template>
-  <main class="chat-shell min-h-dvh lg:h-full overflow-y-auto">
-    <div
-      class="mx-auto flex min-h-dvh w-full max-w-3xl flex-col px-4 py-6 sm:px-6 lg:min-h-full lg:justify-center lg:py-10"
-    >
-      <section class="space-y-5">
-        <div class="space-y-2">
-          <p class="text-[11px] font-semibold uppercase tracking-[0.16em] text-(--app-success)">
-            {{ eyebrow }}
-          </p>
-          <div class="space-y-2">
-            <h1 class="text-2xl font-bold tracking-tight sm:text-3xl">{{ title }}</h1>
-            <p class="max-w-xl text-sm leading-6 text-zinc-500">{{ subtitle }}</p>
-          </div>
-        </div>
-
-        <div class="ui-surface inline-flex rounded-2xl p-1">
-          <button
-            v-for="item in modes"
-            :key="item.id"
-            class="inline-flex items-center justify-center gap-2 rounded-xl px-4 py-2 text-sm font-semibold transition-colors"
-            :class="
-              mode === item.id
-                ? 'bg-(--app-primary-soft) ring-1 ring-(--app-border-strong)'
-                : 'text-zinc-500 hover:text-zinc-300'
-            "
-            @click="setMode(item.id)"
+  <main class="chat-shell min-h-dvh overflow-y-auto lg:h-full">
+    <div class="app-page-shell mx-auto px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
+      <div class="mx-auto max-w-2xl space-y-8">
+        <header class="space-y-4 border-b border-white/8 pb-6">
+          <router-link
+            to="/messages"
+            class="inline-flex items-center gap-1.5 text-sm text-zinc-500 transition-colors hover:text-white"
           >
-            <component :is="item.icon" class="h-4 w-4" :stroke-width="1.9" aria-hidden="true" />
-            {{ item.label }}
-          </button>
-        </div>
+            <ArrowLeft class="h-4 w-4" :stroke-width="2" aria-hidden="true" />
+            Messages
+          </router-link>
+          <div class="space-y-2">
+            <p class="text-[11px] font-semibold uppercase tracking-[0.16em] text-(--app-success)">
+              New conversation
+            </p>
+            <div class="space-y-1.5">
+              <h1 class="text-2xl font-bold tracking-tight sm:text-3xl">{{ title }}</h1>
+              <p class="text-sm leading-6 text-zinc-500">{{ activeModeMeta.description }}</p>
+            </div>
+          </div>
+        </header>
 
-        <AppAlertBanner v-if="error" :message="error" />
-
-        <HomeCreatePanel
-          :active-panel="mode"
-          :dm-pubkey="dmPubkey"
-          :name="name"
-          :description="description"
-          :opening-dm="openingDm"
-          :saving="saving"
-          @update:dm-pubkey="dmPubkey = $event"
-          @update:name="name = $event"
-          @update:description="description = $event"
-          @create-dm="createDM"
-          @create-group="createGroup"
-        />
-
-        <!-- Share options — only shown in DM mode -->
-        <template v-if="mode === 'dm' && identity.pubkeyHex">
-          <div class="flex items-center gap-3">
-            <span class="h-px flex-1 bg-white/8" />
-            <span class="text-[11px] font-semibold uppercase tracking-[0.14em] text-zinc-600"
-              >or share</span
+        <section class="space-y-6">
+          <div class="grid grid-cols-2 gap-2 rounded-2xl border border-white/8 bg-white/[0.02] p-1">
+            <button
+              v-for="item in modes"
+              :key="item.id"
+              type="button"
+              class="inline-flex items-center justify-center gap-2 rounded-xl px-3 py-2.5 text-sm font-semibold transition-all"
+              :class="
+                mode === item.id
+                  ? 'bg-(--app-primary)/15 text-(--app-primary) ring-1 ring-(--app-primary)/30'
+                  : 'text-zinc-500 hover:bg-white/[0.03] hover:text-zinc-300'
+              "
+              @click="setMode(item.id)"
             >
-            <span class="h-px flex-1 bg-white/8" />
+              <component :is="item.icon" class="h-4 w-4 shrink-0" :stroke-width="1.9" aria-hidden="true" />
+              <span class="hidden sm:inline">{{ item.label }}</span>
+              <span class="sm:hidden">{{ item.shortLabel }}</span>
+            </button>
           </div>
 
-          <div class="ui-panel rounded-2xl px-4 py-4 space-y-4">
-            <div class="space-y-1">
-              <div class="flex items-center gap-1.5">
-                <span class="text-[10px] font-semibold uppercase tracking-[0.14em] text-zinc-500"
-                  >Temporary invite</span
-                >
-                <span class="h-px flex-1 bg-white/5" />
-              </div>
-              <p class="text-xs text-zinc-600 leading-5">
-                Safe for WhatsApp and group chats. The link hides your public key, expires
-                automatically, and stops working after it is used once.
-              </p>
-            </div>
+          <AppAlertBanner v-if="error" :message="error" />
 
-            <div class="flex flex-wrap gap-2">
-              <button
-                v-for="option in INVITE_TTL_OPTIONS"
-                :key="option.id"
-                type="button"
-                class="inline-flex items-center gap-1.5 rounded-full px-3 py-1.5 text-xs font-semibold transition-colors"
-                :class="
-                  selectedTtlId === option.id
-                    ? 'bg-(--app-primary-soft) ring-1 ring-(--app-border-strong)'
-                    : 'ui-surface ui-muted hover:ui-soft-text'
-                "
-                @click="selectedTtlId = option.id"
-              >
-                <Clock class="h-3 w-3" :stroke-width="2" aria-hidden="true" />
-                {{ option.label }}
-              </button>
+          <HomeCreatePanel
+            :active-panel="mode"
+            :dm-pubkey="dmPubkey"
+            :name="name"
+            :description="description"
+            :opening-dm="openingDm"
+            :saving="saving"
+            @update:dm-pubkey="dmPubkey = $event"
+            @update:name="name = $event"
+            @update:description="description = $event"
+            @create-dm="createDM"
+            @create-group="createGroup"
+          />
+        </section>
+
+        <section
+          v-if="mode === 'dm' && identity.pubkeyHex"
+          class="space-y-8 border-t border-white/8 pt-8"
+        >
+          <div class="space-y-1.5">
+            <p class="text-[11px] font-semibold uppercase tracking-[0.16em] text-zinc-500">
+              Or connect
+            </p>
+            <h2 class="text-lg font-semibold tracking-tight">Share an invite instead</h2>
+            <p class="max-w-lg text-sm leading-6 text-zinc-500">
+              Generate a temporary link for WhatsApp, Signal, or email. Your public key stays
+              hidden, the link expires, and it works once.
+            </p>
+          </div>
+
+          <div class="space-y-4">
+            <div>
+              <label class="mb-2 block text-sm font-medium text-zinc-300">Link lifetime</label>
+              <div class="flex flex-wrap gap-2">
+                <button
+                  v-for="option in INVITE_TTL_OPTIONS"
+                  :key="option.id"
+                  type="button"
+                  class="inline-flex items-center gap-1.5 rounded-full border px-3.5 py-1.5 text-xs font-medium transition-all"
+                  :class="
+                    selectedTtlId === option.id
+                      ? 'border-(--app-primary)/40 bg-(--app-primary)/15 text-(--app-primary)'
+                      : 'border-white/8 bg-white/[0.02] text-zinc-400 hover:border-white/15 hover:text-white'
+                  "
+                  @click="selectedTtlId = option.id"
+                >
+                  <Clock class="h-3.5 w-3.5" :stroke-width="2" aria-hidden="true" />
+                  {{ option.label }}
+                </button>
+              </div>
             </div>
 
             <AppAlertBanner v-if="inviteError" :message="inviteError" />
@@ -263,74 +280,67 @@ async function createDM() {
               {{ activeInvite ? "Generate new invite" : "Generate invite link" }}
             </PrimaryButton>
 
-            <div v-if="activeInvite" class="space-y-2">
-              <div
-                class="rounded-xl border border-(--app-border) bg-(--app-surface-soft) px-3 py-2.5"
-              >
-                <p class="text-[10px] font-semibold uppercase tracking-[0.14em] ui-muted">
-                  Share this link
+            <div
+              v-if="activeInvite"
+              class="space-y-3 rounded-2xl border border-(--app-primary)/20 bg-(--app-primary)/5 p-4"
+            >
+              <div>
+                <p class="text-[11px] font-semibold uppercase tracking-wider text-zinc-500">
+                  Invite link
                 </p>
-                <p class="mt-1 break-all font-mono text-[11px] ui-soft-text">
+                <p class="mt-2 break-all font-mono text-xs leading-relaxed text-zinc-300">
                   {{ activeInvite.inviteUrl }}
                 </p>
-                <p class="mt-2 text-[11px] ui-muted">
+                <p class="mt-2 text-xs text-zinc-500">
                   Expires in {{ formatInviteExpiry(activeInvite.expiresAt) }}
                 </p>
               </div>
 
               <button
                 type="button"
-                class="w-full inline-flex items-center justify-center gap-1.5 rounded-xl border border-white/7 bg-white/3 px-3 py-2 text-[11px] font-medium text-zinc-400 transition-all hover:bg-white/6 hover:text-zinc-200 hover:border-white/12 active:scale-95"
-                :class="
-                  inviteCopied ? '!text-emerald-400 !border-emerald-500/30 !bg-emerald-500/8' : ''
-                "
-                :aria-label="inviteCopied ? 'Link copied!' : 'Copy temporary invite link'"
+                class="ui-button ui-button-primary inline-flex w-full items-center justify-center gap-2"
+                :class="inviteCopied ? '!bg-emerald-500/15 !text-emerald-300' : ''"
                 @click="copyInviteLink"
               >
-                <Check
-                  v-if="inviteCopied"
-                  class="h-3 w-3 shrink-0"
-                  :stroke-width="2.5"
-                  aria-hidden="true"
-                />
-                <Link2 v-else class="h-3 w-3 shrink-0" :stroke-width="2" aria-hidden="true" />
-                {{ inviteCopied ? "Link copied!" : "Copy temporary invite link" }}
+                <Check v-if="inviteCopied" class="h-4 w-4" :stroke-width="2.5" aria-hidden="true" />
+                <Link2 v-else class="h-4 w-4" :stroke-width="2" aria-hidden="true" />
+                {{ inviteCopied ? "Link copied" : "Copy invite link" }}
               </button>
             </div>
           </div>
 
-          <div class="ui-panel rounded-2xl p-4 space-y-3">
-            <div class="flex items-start justify-between gap-3">
-              <div>
-                <p class="text-xs font-semibold text-zinc-300">Public key</p>
-                <p class="mt-0.5 text-[11px] text-zinc-500 leading-relaxed max-w-[260px]">
-                  Share your full public key for direct adding in Gupt or other Nostr clients.
+          <div class="space-y-3 border-t border-white/8 pt-8">
+            <div class="flex items-start justify-between gap-4">
+              <div class="space-y-1">
+                <h3 class="text-sm font-semibold text-zinc-300">Your public key</h3>
+                <p class="max-w-md text-xs leading-relaxed text-zinc-500">
+                  For people already on Gupt or other Nostr clients who want to add you directly.
                 </p>
               </div>
               <button
                 type="button"
-                class="ui-icon-button inline-flex items-center gap-1 text-[10px] font-medium uppercase tracking-[0.05em] px-2.5 py-1 rounded-full transition-colors shrink-0"
+                class="inline-flex shrink-0 items-center gap-1.5 rounded-full border px-3 py-1.5 text-xs font-semibold transition-colors"
                 :class="
                   copied
-                    ? 'bg-emerald-500/10 text-emerald-400'
-                    : 'bg-white/5 text-zinc-400 hover:bg-white/10 hover:text-zinc-200'
+                    ? 'border-emerald-500/30 bg-emerald-500/10 text-emerald-300'
+                    : 'border-white/8 bg-white/[0.02] text-zinc-400 hover:border-white/15 hover:text-white'
                 "
                 @click="copyPubkey"
               >
-                <Check v-if="copied" class="h-3 w-3" :stroke-width="2.5" aria-hidden="true" />
-                <Copy v-else class="h-3 w-3" :stroke-width="2" aria-hidden="true" />
+                <Check v-if="copied" class="h-3.5 w-3.5" :stroke-width="2.5" aria-hidden="true" />
+                <Copy v-else class="h-3.5 w-3.5" :stroke-width="2" aria-hidden="true" />
                 {{ copied ? "Copied" : "Copy" }}
               </button>
             </div>
 
             <p
-              class="rounded-xl border border-(--app-border) bg-(--app-surface-soft) px-3 py-2.5 font-mono text-[11px] leading-5 ui-soft-text break-all select-all transition-colors hover:border-(--app-border-strong)"
+              class="rounded-xl border border-white/8 bg-black/20 px-3 py-3 font-mono text-[11px] leading-5 text-zinc-400 break-all select-all"
             >
               {{ identity.pubkeyHex }}
             </p>
           </div>
-        </template>
-      </section>
+        </section>
+      </div>
     </div>
   </main>
 </template>
